e15bff6d9ee90931b601f14f9d575c0426040b22
Tarik Guney <atarikguney@gmail.com>
2017-09-14 22:51:32 -0600

    Converted the example to channel experiment

--- main.go
+++ main.go
@@ -1,45 +1,28 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-	"time"
-)
+import "fmt"
+import "time"
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Experimenting channels between goroutines
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	go func() {
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		// Now handleConn does not exit, but it is executed concurrently
-		// so the loop continues to accept connections and handle.
-		// don't forget that this is a inifitely running server.
-		go handleConn(conn)
+		fmt.Println(<-squares)
+		time.Sleep(1 * time.Second)
 	}
-}
-
-func handleConn(c net.Conn) {
-	input := bufio.NewScanner(c)
-	for input.Scan() {
-		echo(c, input.Text(), 1*time.Second)
-	}
-	c.Close()
-}
-
-func echo(c net.Conn, shout string, delay time.Duration) {
-	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
-	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", shout)
-	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
